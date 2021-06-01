02951188d6974516054c6ccba3db8ab5a32a326b
Elvira Nafikova <81649305+Nielra@users.noreply.github.com>
2021-06-01 22:47:54 +0500

    Разделение на функции, указатели

--- Ball.cpp
+++ Ball.cpp
@@ -2,6 +2,10 @@
 //#include "..\proba_lib\Pugovkina_Lib.h"
 
 void MoveBall();
+void DrawBall (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor);
+void PhysicsBall (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt);
+void ControlBall1 (int* vx, int* vy);
+void ControlBall2 (int* vx, int* vy);
 
 int main()
     {
@@ -14,92 +18,102 @@ int main()
 
 void MoveBall()
     {
-    int x = 0, y = 0,
-        vx =5, vy = 3;
+    int x1  = 0, y1  = 0,
+        vx1 = 5, vy1 = 3,
+        ax1 = 0, ay1 = 0;
 
-    int x1 = 100, y1 = 100,
-        vx1 = 3,  vy1 = 5;
+    int x2  = 100, y2  = 100,
+        vx2 = 3,   vy2 = 5,
+        ax2 = 0,   ay2 = 0;
 
     int dt = 1;
 
-    txSetColor (RGB (x, y, x + y), 2);
-    txSetFillColor (RGB (x/2, y/2, x/2 + y/2));
-
     while (!txGetAsyncKeyState (VK_ESCAPE))
         {
-        txSetColor (RGB (x, y, 128), 2);
-        txSetFillColor (RGB (x/2, y/2, 128));
+        DrawBall (x1, y1, vx1, vy1, TX_LIGHTRED, TX_RED);
+        PhysicsBall (&x1, &y1, &vx1, &vy1, ax1, ay1, dt);
+        ControlBall1 (&vx1, &vy1);
 
-        txCircle (x, y, 20);
-        //Flowers (x, y, TX_YELLOW, TX_RED);
-
-        x += vx * dt;
-        y += vy * dt;
-
-        if (x > 800)
-            {
-            vx = -vx;
-            x = 800;
-            }
-
-        if (y > 600)
-            {
-            vy = -vy;
-            y = 600;
-            }
-
-        if (x < 0)
-            {
-            vx = -vx;
-            x = 0;
-            }
-
-        if (y < 0)
-            {
-            vy = -vy;
-            y = 0;
-            }
-
-        txSetColor (RGB (x1, y1, 128), 2);
-        txSetFillColor (RGB (x1/2, y1/2, 128));
-
-        txCircle (x1, y1, 20);
-        //Flowers (x, y, TX_YELLOW, TX_RED);
-
-        x1 += vx1 * dt;
-        y1 += vy1 * dt;
-
-        if (x1 > 800)
-            {
-            vx1 = -vx1;
-            x1 = 800;
-            }
-
-        if (y1 > 600)
-            {
-            vy1 = -vy1;
-            y1 = 600;
-            }
-
-        if (x1 < 0)
-            {
-            vx1 = -vx1;
-            x1 = 0;
-            }
-
-        if (y1 < 0)
-            {
-            vy1 = -vy1;
-            y1 = 0;
-            }
-
-        if (txGetAsyncKeyState (VK_RIGHT)) vx1 ++;
-        if (txGetAsyncKeyState (VK_LEFT))  vx1 --;
-        if (txGetAsyncKeyState (VK_UP))    vy1 --;
-        if (txGetAsyncKeyState (VK_DOWN))  vy1 ++;
-
-        if (txGetAsyncKeyState (VK_SPACE)) vx1 = vy1 = 0;
+        DrawBall (x2, y2, vx2, vy2, TX_LIGHTGREEN, TX_GREEN);
+        PhysicsBall (&x2, &y2, &vx2, &vy2, ax2, ay2, dt);
+        ControlBall2 (&vx2, &vy2);
 
         txSleep (10);
         }
+    }
+
+
+//-----------------------------------------------------------------------------
+
+void DrawBall (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor)
+    {
+    txSetColor (color, 2);
+    txSetFillColor (fillcolor);
+
+    txCircle (x, y, 20);
+    txLine (x, y, x + vx * 5, y + vy * 5);
+    txCircle (x + vx * 5, y + vy * 5, 3);
+    //Flowers (x, y, TX_YELLOW, TX_RED);
+
+    }
+
+//-----------------------------------------------------------------------------
+
+void PhysicsBall (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt)
+    {
+    *vx = *vx + ax * dt;
+    *vy = *vy + ay * dt;
+
+    *x = *x + *vx * dt;
+    *y = *y + *vy * dt;
+
+    if (*x > 800)
+        {
+        *vx = - (*vx);
+        *x = 800;
+        }
+
+    if (*y > 600)
+        {
+        *vy = - (*vy);
+        *y = 600;
+        }
+
+    if (*x < 0)
+        {
+        *vx = - (*vx);
+        *x = 0;
+        }
+
+    if (*y < 0)
+        {
+        *vy = - (*vy);
+        *y = 0;
+        }
+    }
+
+//-----------------------------------------------------------------------------
+
+void ControlBall1 (int* vx, int* vy)
+    {
+    if (txGetAsyncKeyState (VK_RIGHT)) (*vx) ++;
+    if (txGetAsyncKeyState (VK_LEFT))  (*vx) --;
+    if (txGetAsyncKeyState (VK_UP))    (*vy) --;
+    if (txGetAsyncKeyState (VK_DOWN))  (*vy) ++;
+
+    if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+
+    }
+
+//-----------------------------------------------------------------------------
+
+void ControlBall2 (int* vx, int* vy)
+    {
+    if (txGetAsyncKeyState ('D')) (*vx) ++;
+    if (txGetAsyncKeyState ('A')) (*vx) --;
+    if (txGetAsyncKeyState ('W')) (*vy) --;
+    if (txGetAsyncKeyState ('S')) (*vy) ++;
+
+    if (txGetAsyncKeyState ('Z')) *vx = *vy = 0;
+
     }
